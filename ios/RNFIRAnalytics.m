f06af568dae1b1a5929cf009c7eb6f3dd4f2abb9
Sathyanarayana <sathyanarayana.an@unicity.com>
2019-01-03 09:39:17 +0530

    initial commit

---
+++ ios/RNFIRAnalytics.m
@@ -0,0 +1,47 @@
+#import "RNFIRAnalytics.h"
+#import <FirebaseAnalytics/FirebaseAnalytics.h>
+
+@implementation RNFIRAnalytics
+
+RCT_EXPORT_MODULE()
+
+@synthesize bridge = _bridge;
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+RCT_EXPORT_METHOD(setUserId: (NSString*) userId)
+{
+  [FIRAnalytics setUserID:userId];
+}
+
+RCT_EXPORT_METHOD(setUserProperty: (NSString*)name property: (NSString*)property)
+{
+  [FIRAnalytics setUserPropertyString:property forName:name];
+}
+
+RCT_EXPORT_METHOD(logEvent: (NSString*)name property: (NSDictionary*)parameters)
+{
+  [FIRAnalytics logEventWithName:name parameters:parameters];
+}
+
+RCT_EXPORT_METHOD(setScreenName: (NSString*)name)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [FIRAnalytics setScreenName:name screenClass:nil];
+  });
+}
+
+RCT_EXPORT_METHOD(setEnabled: (BOOL)enabled)
+{
+  [[FIRAnalyticsConfiguration sharedInstance] setAnalyticsCollectionEnabled:enabled];
+}
+
+@end
